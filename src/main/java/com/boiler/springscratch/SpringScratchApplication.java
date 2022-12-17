1bc769d7cc3af88a67b8981d8758db6da031e282
redhaanggara21 <redhabayuanggara@gmail.com>
2022-12-17 14:57:13 +0700

    saving idea

---
+++ src/main/java/com/boiler/springscratch/SpringScratchApplication.java
@@ -0,0 +1,13 @@
+package com.boiler.springscratch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringScratchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringScratchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boiler/springscratch/SpringScratchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boiler.springscratch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringScratchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
